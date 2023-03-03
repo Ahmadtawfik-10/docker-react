d5c30eb0fe96791206cbc2a8160620e9fe51573c
Ahmad Tawfik <ahmadtawfik.at1@gmail.com>
2023-03-03 17:10:28 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi from a volume
+          Ahmad Tawfik
         </p>
         <a
           className="App-link"
