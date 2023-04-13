dea27b81d1e1d462265b2b1a4cca987516349dd5
Ahmad Tawfik <ahmadtawfik.at1@gmail.com>
2023-04-13 14:22:23 +0200

    c

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahmad Tawfik learning DOCKERR
+          Ahmad Tawfik learning DOCKER
         </p>
         <a
           className="App-link"
